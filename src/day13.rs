d7fbcd3b65ac54abb2468796f40cffdec2c463bb
mwyrebski <mwyrebski@users.noreply.github.com>
2021-04-10 00:09:35 +0200

    Solution for day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -1,7 +1,13 @@
 #[derive(PartialEq, Debug)]
+struct Bus {
+    id: i64,
+    offset: usize,
+}
+
+#[derive(PartialEq, Debug)]
 struct Notes {
     depart_timestamp: u32,
-    buses: Vec<u32>,
+    buses: Vec<Bus>,
 }
 
 fn parse_input<'a>(input: &str) -> Notes {
@@ -11,8 +17,8 @@ fn parse_input<'a>(input: &str) -> Notes {
         .next()
         .unwrap()
         .split(',')
-        .filter(|x| x != &"x")
-        .map(|x| x.parse::<u32>().unwrap())
+        .enumerate()
+        .filter_map(|(offset, bus_id)| bus_id.parse::<i64>().ok().map(|id| Bus { id, offset }))
         .collect::<Vec<_>>();
     Notes {
         depart_timestamp,
@@ -21,7 +27,7 @@ fn parse_input<'a>(input: &str) -> Notes {
 }
 
 #[inline]
-fn div_ceil(num: u32, div: u32) -> u32 {
+fn div_ceil(num: i64, div: i64) -> i64 {
     let t = num / div;
     if t % div > 0 {
         t + 1
@@ -30,25 +36,53 @@ fn div_ceil(num: u32, div: u32) -> u32 {
     }
 }
 
-fn part1(notes: &Notes) -> u32 {
+fn part1(notes: &Notes) -> i64 {
     let (id, diff) = notes
         .buses
         .iter()
-        .map(|id| {
-            let id = *id;
-            let freq = div_ceil(notes.depart_timestamp, id);
-            let timestamp = freq * id;
-            (id, timestamp - notes.depart_timestamp)
+        .map(|bus| {
+            let depart = notes.depart_timestamp as i64;
+            let freq = div_ceil(depart, bus.id);
+            let timestamp = freq * bus.id;
+            (bus.id, timestamp - depart)
         })
         .min_by_key(|(_, diff)| *diff)
         .unwrap();
     id * diff
 }
 
+// source: https://github.com/simon-andrews/rust-modinverse/
+fn egcd(a: i64, b: i64) -> (i64, i64, i64) {
+    if a == 0 {
+        (b, 0, 1)
+    } else {
+        let (g, x, y) = egcd(b % a, a);
+        (g, y - (b / a) * x, x)
+    }
+}
+
+fn part2(notes: &Notes) -> i64 {
+    let ns: i64 = notes.buses.iter().map(|bus| bus.id).product();
+
+    let xs: i64 = notes
+        .buses
+        .iter()
+        .map(|bus| {
+            let n = ns / bus.id;
+            let (_, m, _) = egcd(n, bus.id);
+            let x = bus.offset as i64 * n * m;
+            x
+        })
+        .sum();
+
+    ns - xs.rem_euclid(ns)
+}
+
 pub fn run() {
     let input = include_str!("input/day13.txt");
     let notes = parse_input(input);
     println!("Day 13/1: {}", part1(&notes));
+    println!("Day 13/2: {}", part2(&notes));
 }
 
 #[cfg(test)]
@@ -62,11 +96,12 @@ mod tests {
 
     #[test]
     fn test_day13_parse_input() {
+        let b = |id, offset| -> Bus { Bus { id, offset } };
         let notes = parse_input(SAMPLE1);
         assert_eq!(
             Notes {
                 depart_timestamp: 939,
-                buses: vec![7, 13, 59, 31, 19]
+                buses: vec![b(7, 0), b(13, 1), b(59, 4), b(31, 6), b(19, 7)]
             },
             notes
         );
@@ -76,5 +111,21 @@ mod tests {
     fn test_day13_part1_sample1() {
         let notes = &parse_input(SAMPLE1);
         assert_eq!(295, part1(notes));
+    }
+
+    #[test]
+    fn test_day13_part2_sample1() {
+        let notes = &parse_input(SAMPLE1);
+        assert_eq!(1068781, part2(notes));
+    }
+
+    #[test]
+    //#[ignore]
+    fn test_day13_part2_more_samples() {
+        assert_eq!(3417, part2(&parse_input("0\n17,x,13,19")));
+        assert_eq!(754018, part2(&parse_input("0\n67,7,59,61")));
+        assert_eq!(779210, part2(&parse_input("0\n67,x,7,59,61")));
+        assert_eq!(1261476, part2(&parse_input("0\n67,7,x,59,61")));
+        assert_eq!(1202161486, part2(&parse_input("0\n1789,37,47,1889")));
     }
 }
